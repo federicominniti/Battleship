c3c400fae3641b57178aa8ca8d1bf51d9f3b880b
tom <balditommaso22@gmail.com>
2022-02-04 18:44:42 +0100

    servelet login/register with controls

--- BattleShip/src/main/java/it/unipi/dii/inginf/dsmt/battleship/servlet/LoginServlet.java
+++ BattleShip/src/main/java/it/unipi/dii/inginf/dsmt/battleship/servlet/LoginServlet.java
@@ -1,5 +1,8 @@
 package it.unipi.dii.inginf.dsmt.battleship.servlet;
 
+import it.unipi.dii.inginf.dsmt.battleship.model.User;
+import it.unipi.dii.inginf.dsmt.battleship.persistence.LevelDBDriver;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -7,6 +10,9 @@ import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", value = "/login")
 public class LoginServlet extends HttpServlet {
+
+    LevelDBDriver levelDBDriver = LevelDBDriver.getInstance();
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -14,6 +20,23 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        User user = levelDBDriver.login(username, password);
+        if (user == null) {
+            String message = "Wrong username or password.";
+            request.setAttribute("msg", message);
+            String targetJSP = request.getContextPath() + "/index.jsp";
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher(targetJSP);
+            requestDispatcher.forward(request, response);
+        } else {
+            HttpSession session = request.getSession(true);
+            if (session.getAttribute("logged") == null) {
+                session.setAttribute("logged", user);
+            }
+            String targetJSP = request.getContextPath() + "/pages/homepage.jsp";
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher(targetJSP);
+            requestDispatcher.forward(request, response);
+        }
     }
 }
