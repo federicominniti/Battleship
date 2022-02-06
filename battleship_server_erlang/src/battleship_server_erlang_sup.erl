88ced7d2a5246392370b6626092bda935e9e6153
Federico Minniti <93187746+federicominniti@users.noreply.github.com>
2022-02-06 12:25:07 +0100

    added erlang

---
+++ battleship_server_erlang/src/battleship_server_erlang_sup.erl
@@ -0,0 +1,25 @@
+%%%-------------------------------------------------------------------
+%% @doc battleship_server_erlang top level supervisor.
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(battleship_server_erlang_sup).
+
+-behaviour(supervisor).
+
+-export([start_link/0]).
+
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+init([]) ->
+    SupFlags = #{strategy => one_for_all,
+                 intensity => 0,
+                 period => 1},
+    ChildSpecs = [],
+    {ok, {SupFlags, ChildSpecs}}.
+
