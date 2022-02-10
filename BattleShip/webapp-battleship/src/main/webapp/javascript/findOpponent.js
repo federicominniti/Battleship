85c41b1ee7b05de595db565d2fd6f5154182afb5
Federico Minniti <93187746+federicominniti@users.noreply.github.com>
2022-02-10 09:34:09 +0100

    Added findOpponent.js

---
+++ BattleShip/webapp-battleship/src/main/webapp/javascript/findOpponent.js
@@ -0,0 +1,99 @@
+//WEBSOCKET INITIALIZATION
+
+initWebSocket(username);
+
+// Publish the user oline and available for a new game
+waitForSocketConnection(ws, function(){
+    sendWebSocket(new Message("user_online", null , username, null));
+});
+
+//------------------------
+
+//@Override from webSocket.js
+ws.onmessage = function (event){
+    var jsonString = JSON.parse(event.data);
+    var sender = jsonString.sender;
+    console.log("Message received from the message server");
+
+    if (jsonString.type === 'game_request') {
+
+        let table = document.getElementById("gameRequests");
+        for(let step = table.childNodes.length - 1; step > 1; step--) {
+            let tr = table.childNodes.item(step);
+            let td = tr.firstChild;
+
+            // Duplicated request
+            if(td.textContent === sender) {
+                return;
+            }
+        }
+
+        // Otherwise, the game request is added to the list
+        let tr = document.createElement("tr");
+
+        let tdText = document.createElement("td");
+        tdText.textContent = sender;
+        tr.appendChild(tdText);
+
+        let button = document.createElement("button");
+        button.textContent = "Accept";
+        button.onclick = function() { sendGameRequestAccepted(sender); }
+
+        let tdButton = document.createElement("td");
+        tdButton.appendChild(button);
+        tr.appendChild(tdButton);
+
+        table.appendChild(tr);
+    }
+    else if (jsonString.type === 'game_request_accepted')   // My opponent has accepted my game request
+    {
+        // If my opponent accepted a game request for a game that is not
+        // the one I have selected there is an error, return
+        if(jsonString.data !== gameName) return;
+
+        // Otherwise, the game starts
+        if (gameName === "connectFour")
+            goToConnectFourGame("yellow", sender);
+        else if (gameName === "ticTacToe")
+        {
+            goToTicTacToeGame(username, sender);
+        }
+    } else if(jsonString.type === 'updated_online_users') // The list of online users has changed
+    {
+        let list = jsonString.data;
+        let table = document.getElementById("onlineUsers");
+
+        while(table.childNodes.length > 2) {
+            table.removeChild(table.lastChild);
+        }
+
+        for (let i = 0; i < list.length; i++) {
+            let tr = document.createElement("tr");
+            let td = document.createElement("td");
+            td.textContent = list[i];
+            td.addEventListener("click", function () {
+                sendGameRequest(td.textContent);
+            })
+
+            tr.appendChild(td);
+            table.appendChild(tr);
+        }
+    } else if(jsonString.type === 'remove_requests') {  // A precedent request is not still valid because a user might not be online anymore
+        let table = document.getElementById("gameRequests");
+
+        for(let i = table.childNodes.length - 1; i > 1; i--) {
+            let tr = table.childNodes.item(i);
+            let td = tr.firstChild;
+            if(td.textContent === jsonString.data) {
+                table.removeChild(tr);
+            }
+        }
+    } else if(jsonString.type === 'logged_sender_error'){
+        alert("You are logged in from another device!")
+        document.location.href = './logout';
+    }
+};
+
+function sendRequestForAGame(possibleOpponent) {
+    sendWebSocket(new Message('game_request', null, username, possibleOpponent))
+}
