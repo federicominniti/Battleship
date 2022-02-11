02190f18d96edd4e3f3db466bcf16c019552203a
matteodelseppia <matteodelseppia.cc@gmail.com>
2022-02-11 09:39:02 +0100

    added filter for reloads

---
+++ BattleShip/webapp-battleship/src/main/java/it/unipi/dii/inginf/dsmt/battleship/filter/ReloadFilter.java
@@ -0,0 +1,36 @@
+package it.unipi.dii.inginf.dsmt.battleship.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebFilter(filterName = "AuthFilter", urlPatterns = {"/pages/homepage.jsp"})
+public class ReloadFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpSession session = request.getSession();
+
+        if (session.getAttribute("numReloads") == null) {
+            session.setAttribute("numReloads", 0);
+        } else {
+            session.setAttribute("numReloads", 1);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    public void destroy() {
+
+    }
+}
