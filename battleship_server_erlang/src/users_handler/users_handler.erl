d95c7a4a99e74fdc189393fc3bde2ec3f0d317d4
Federico Minniti <93187746+federicominniti@users.noreply.github.com>
2022-02-16 13:09:07 +0100

    solved bugs

--- battleship_server_erlang/src/users_handler/users_handler.erl
+++ battleship_server_erlang/src/users_handler/users_handler.erl
@@ -13,8 +13,8 @@
 
 init_server() ->
 	Server = self(),
-  	BattleShipPid = spawn( fun() -> battleship_daemon(Server, #{}) end ),
-  	RandomOpponentPid = spawn( fun() -> random_opponent_daemon(null) end ),
+	RandomOpponentPid = spawn( fun() -> random_opponent_daemon(Server, null) end ),
+  	BattleShipPid = spawn( fun() -> battleship_daemon(Server, RandomOpponentPid, #{}) end ),
   	server_loop(BattleShipPid, RandomOpponentPid).
 
 % It handles mainly 2 types of interaction: online users' manipulation and search a random user
@@ -23,13 +23,17 @@ server_loop(BattleShipPid, RandomOpponentPid) ->
 		{From, {update_online_user, Operation} } ->
 			BattleShipPid ! {From, Operation},
 			server_loop(BattleShipPid, RandomOpponentPid);
+			
 		{From, search_random_opponent} ->
 			RandomOpponentPid ! {From, search_random_opponent},
+			BattleShipPid ! {From, user_in_game},
 			server_loop(BattleShipPid, RandomOpponentPid);
+			
     	{From, stop} ->
       		BattleShipPid ! {self(), stop},
 			RandomOpponentPid ! {self(), stop},
       		From ! {ok};
+			
     	_ ->  
       		server_loop(BattleShipPid, RandomOpponentPid)
   	end.
@@ -37,21 +41,33 @@ server_loop(BattleShipPid, RandomOpponentPid) ->
 % Random user research: 
 % 	1st user is saved and wait for an opponent 
 % 	2nd user is matched with the previous user and the demon send the needed message to allow users to start the game  
-random_opponent_daemon(User) ->
+random_opponent_daemon(Server, User) ->
 	receive
 		{From, search_random_opponent} ->
 			if
 				(User == null) -> 
 					NewUser = From,
-					random_opponent_daemon(NewUser);
+					random_opponent_daemon(Server, NewUser);
 				true -> 
 					MessageUser = jsx:encode(#{<<"type">> => <<"battleship_accepted">>, <<"sender">> => From}),
 					User ! MessageUser,
 					MessageFrom = jsx:encode(#{<<"type">> => <<"battleship_accepted">>, <<"sender">> => User}),
 					From ! MessageFrom,
 					NewUser = null,
-					random_opponent_daemon(NewUser)
-			end
+					random_opponent_daemon(Server, NewUser)
+			end;
+			
+		{From, stop_search} ->
+			if
+				(From == User) ->
+					NewUser = null;
+				true -> 
+					io:format("Error on stop random users search\n"),
+					NewUser = User
+			end,
+			random_opponent_daemon(Server, NewUser);
+	    {Server, stop} ->
+	    	ok
 	end.
 
 % Manipulation of online users. We have 3 types of interactions:
@@ -60,33 +76,36 @@ random_opponent_daemon(User) ->
 %	2 user_in_game: a user start a game with another -> the daemon change the state of the two processes individually and send an update 
 %					to the others processes to inform them that it is not available to receive requests and another massage to remove all 
 %					the request of the user that started a game
-battleship_daemon(Server, OnlineUsers) ->
+battleship_daemon(Server, RandomOpponentPid, OnlineUsers) ->
 	receive
     	{From, add} ->
 			UpdatedOnlineUsers = maps:put(From, in_lobby, OnlineUsers),
       	  	send_all(UpdatedOnlineUsers),
       	  	io:format("New user available: ~s\n", [From]),
       	  	io:format("Online users: ~w ~n", [UpdatedOnlineUsers]),
-      	  	battleship_daemon(Server, UpdatedOnlineUsers);
+      	  	battleship_daemon(Server, RandomOpponentPid, UpdatedOnlineUsers);
 	  
 		{From, user_in_game} ->
 			UpdatedOnlineUsers = maps:put(From, in_game, OnlineUsers),
 			send_all(UpdatedOnlineUsers),
 			update_all(UpdatedOnlineUsers, From),
 			io:format("~s starts a game\n", [From]),
-			battleship_daemon(Server, UpdatedOnlineUsers);
+			battleship_daemon(Server, RandomOpponentPid, UpdatedOnlineUsers);
 		
     	{From, remove} ->
+			RandomOpponentPid ! {From, stop_search},
       	  	UpdatedOnlineUsers = maps:remove(From, OnlineUsers),
       	  	send_all(UpdatedOnlineUsers),
      	   	update_all(UpdatedOnlineUsers, From),
      	   	io:format("~s is now offline\n", [From]),
     		io:format("Online users: ~w ~n", [UpdatedOnlineUsers]),
-    		battleship_daemon(Server, UpdatedOnlineUsers);
+    		battleship_daemon(Server, RandomOpponentPid, UpdatedOnlineUsers);
+			
     	{Server, stop} ->
      	   	ok;
+			
     	_ -> 
-			battleship_daemon(Server, OnlineUsers)
+			battleship_daemon(Server, RandomOpponentPid, OnlineUsers)
 	end.
 
 
